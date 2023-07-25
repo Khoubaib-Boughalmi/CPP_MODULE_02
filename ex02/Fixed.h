7289550270cbf4b5411c45e90fb38241a4e29452
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-07-25 18:47:35 +0100

    finished ex02

---
+++ ex02/Fixed.h
@@ -0,0 +1,45 @@
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int fixedPoint;
+        const static int fractionalBits = 8;
+    public:
+        Fixed();
+        Fixed(const int intValue);
+        Fixed(const float floatValue);
+        Fixed(const Fixed& other);
+        ~Fixed();
+        Fixed& operator=(const Fixed& other);
+        bool operator>(const Fixed& other) const;
+        bool operator<(const Fixed& other) const;
+        bool operator>=(const Fixed& other) const;
+        bool operator<=(const Fixed& other) const;
+        bool operator==(const Fixed& other) const;
+        bool operator!=(const Fixed& other) const;
+        Fixed operator+(const Fixed& other);
+        Fixed operator-(const Fixed& other);
+        Fixed operator*(const Fixed& other);
+        Fixed operator/(const Fixed& other);
+        Fixed operator++(int);
+        Fixed operator--(int);
+        Fixed operator++(void);
+        Fixed operator--(void);
+        int getRawBits( void );
+        void setRawBits( int const raw );
+        float toFloat( void ) const;
+        int toInt( void ) const;
+        static Fixed min(Fixed& f1, Fixed& f2);
+        static Fixed min(const Fixed& f1, const Fixed& f2);
+        static Fixed max(Fixed& f1, Fixed& f2);
+        static Fixed max(const Fixed& f1, const Fixed& f2);
+};
+
+std::ostream& operator<<(std::ostream& out,const Fixed& fixedObj);
+
+#endif
