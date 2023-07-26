d4079559ad6c8a607bde1fedcef3cf3c8260e8a4
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-07-26 01:04:15 +0100

    finished ex03

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -4,12 +4,7 @@
 float calculate_area(Point const a, Point const b, Point const c)
 {
     // (1/2) (x1(y2 - y3) + x2(y3 - y1) + x3(y1 - y2))
-    // float area1 = std::abs((1/2) * (a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat()) + b.getX().toFloat() * (c.getY().toFloat() - a.getY().toFloat()) + c.getX().toFloat() * (a.getY().toFloat() - b.getY().toFloat()))); // abc
-    float area1 = std::abs((1/2) * (a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat())) ); // abc
-    std::cout << a.getX().toFloat() << std::endl;
-    std::cout << b.getY().toFloat() << std::endl;
-    std::cout << c.getY().toFloat() << std::endl;
-    std::cout << "AREA1 " << area1 << std::endl;
+    float area1 = std::abs(0.5 * (a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat()) + b.getX().toFloat() * (c.getY().toFloat() - a.getY().toFloat()) + c.getX().toFloat() * (a.getY().toFloat() - b.getY().toFloat()))); // abc
     return area1;
 
 }
@@ -21,10 +16,6 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
     float area3 = calculate_area(a, c, point);//acp
     float area4 = calculate_area(b, c, point);//bcp
     if(area2 + area3 + area4 == area1)
-    {
-        std::cout << area3 << std::endl;
-        std::cout << area4 << std::endl;
         return (1);
-    }
     return (0);
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,10 +1,10 @@
 #include "Point.h"
 
 int main( void ) {
-    Point const  a(0,0);
-    Point const  b(5,0);
-    Point const  c(7,1);
-    Point const  point(-40,10);
+    Point const  a(2,2);
+    Point const  b(6,3);
+    Point const  c(4,7);
+    Point const  point(0.2,3.9);
     std::cout << bsp(a, b, c, point) << std::endl;
 }
 
