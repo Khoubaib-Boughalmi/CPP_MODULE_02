9a07589e10dbb862cf2b33a36465695ab13e77b6
Khoubaib-Boughalmi <khoubaib.kb99@gmail.com>
2023-07-28 20:20:07 +0100

    finished

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,7 +4,10 @@ int main( void ) {
     Point const  a(-0.4,0);
     Point const  b(5.2,1.2);
     Point const  c(2.2,4.4);
-    Point const  point(0.3,0.5);
-    std::cout << bsp(a, b, c, point) << std::endl;
+    Point const  point(-0.3,0.5);
+    if(bsp(a, b, c, point))
+        std::cout << "Point is inside the triangle" << std::endl;
+    else
+        std::cout << "Point is NOT inside the triangle" << std::endl;
 }
 
